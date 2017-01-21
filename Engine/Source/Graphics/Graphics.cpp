f45addc8863af04a5a32e42ece05713d24398ec4
Jason Bogert <boagz4@gmail.com>
2017-01-21 09:09:14 -0800

    First step to figuring out how to implement a graphics engine for now is for me to being just writing low level opengl functions within 'graphics'. This makes it easier for me to follow instead of having everything abstracted away. Currently just go a triangle on screen with the gl functions.

--- Engine/Source/Graphics/Graphics.cpp
+++ Engine/Source/Graphics/Graphics.cpp
@@ -13,10 +13,22 @@ void Graphics::Draw()
 
 void Graphics::InitializeBuffers()
 {
-	MyOpenGL::InitializeGLBuffers(triangle);
+	glGenBuffers(1, &vertexBufferID);
+	glGenBuffers(1, &indexBufferID);
+
+	glBindBuffer(GL_ARRAY_BUFFER, vertexBufferID);
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, indexBufferID);
+
+	glBufferData(GL_ARRAY_BUFFER, cMaxBufferSize, nullptr, GL_DYNAMIC_DRAW);
+	glBufferData(GL_ELEMENT_ARRAY_BUFFER, cMaxBufferSize, nullptr, GL_DYNAMIC_DRAW);
+
+	glEnableVertexAttribArray(0);
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, sizeof(GLfloat) * 3, nullptr);
 }
 
 void Graphics::Update(Entity& obj)
 {
+	glBufferSubData(GL_ARRAY_BUFFER, 0, sizeof(BlazeFramework::Math::Vertex3D) * 3, &triangle.front());
+	glBufferSubData(GL_ELEMENT_ARRAY_BUFFER, 0, sizeof(GLushort) * 3, &indicies.front());
 	glDrawElements(GL_TRIANGLES, 3, GL_UNSIGNED_SHORT, 0);
 }
--- Engine/Source/Graphics/Graphics.h
+++ Engine/Source/Graphics/Graphics.h
@@ -1,4 +1,5 @@
 #pragma once
+#include "GL/glew.h"
 #include "Math/Vertex3D/Vertex3D.h"
 #include "DataStructures/Vector/Vector.h"
 #include "GameWorld/Entity.h"
@@ -13,11 +14,20 @@ public:
 	void Update(Entity& obj);
 
 private:
+	static const uint16 cMaxBufferSize = 1024;
+	GLuint vertexBufferID;
+	GLuint indexBufferID;
+
 	Vector<BlazeFramework::Math::Vertex3D> triangle
 	{
 		BlazeFramework::Math::Vertex3D(+0.0f, +0.2f, 0.0f),
 		BlazeFramework::Math::Vertex3D(-0.1f, 0.0f, 0.0f),
 		BlazeFramework::Math::Vertex3D(+0.1f, 0.0f, 0.0f)
+	};
+
+	Vector<GLushort> indicies
+	{
+		0, 1, 2
 	};
 };
 
