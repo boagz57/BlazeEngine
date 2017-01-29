2931164a923a7a352f36e54295e8f52c96455512
Jason Bogert <boagz4@gmail.com>
2017-01-29 12:42:49 -0800

    Corrected Test for ship in EngineTester so that initalization can be tested.

--- EngineTester/Source/SubSystems/GameWorld/GameEntities/ShipTest.cpp
+++ EngineTester/Source/SubSystems/GameWorld/GameEntities/ShipTest.cpp
@@ -1,7 +1,9 @@
 #include "gtest/gtest.h"
+#include "Math/Vector2D/Vector2D.h"
 #include "GameWorld/GameEntities/Ship.h"
 
 TEST(GameWorldSubSystem, ShipClass)//TODO: setup to test initialize and shutdown functions 
 {
 	BlazeGameWorld::Ship ship;
+	EXPECT_TRUE(ship.Initialize(BlazeFramework::Math::Vector2D(0.0f, 1.0f)));
 }
