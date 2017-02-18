54382998c5dfac230a31dcda25557ded8d89c0db
Jason Bogert <boagz4@gmail.com>
2017-02-18 05:54:32 -0800

    Just added to a comment. Wanted to commit before starting rendering system refactoring.

--- Engine/Source/SubSystems/Components/Component.h
+++ Engine/Source/SubSystems/Components/Component.h
@@ -1,9 +1,12 @@
 #pragma once
 
 /*Components are attached to entities via the bitwise operations below. Every
-Entity bitmasks will be set by these operations and then every system will 
-compare an entity's bitmask to the system's bitmask. If a match is found then 
-the system will except that entity and work with the needed components.
+Entity bitmask will be set by these operations and that will determine an 
+Entity's properties. So an Entity with a PostionComponent attached would have
+a bitmask of .....0001, and an Entity with Position and velocity would have
+a bitmask of .....0101. Then every system will compare an entity's bitmask 
+to the system's bitmask. If a match is found then the system will except 
+that entity and work with the needed components.
 */
 
 enum Component
