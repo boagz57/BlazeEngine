33b0c84de0e825bb982bbd741100e21e2a417cfd
Jason Bogert <boagz4@gmail.com>
2017-02-21 19:12:11 -0800

    Refactor: Made Macro.h file more readable.

--- Engine/Source/Universal/Macro.h
+++ Engine/Source/Universal/Macro.h
@@ -6,16 +6,21 @@
 #define DLL  \
 	__declspec(dllexport)
 
-#define _PROFILE true
+#define RUNTIME_ASSERT \
+	BOOST_ASSERT_MSG 
 
-#define RUNTIME_ASSERT BOOST_ASSERT_MSG 
-#define COMPILETIME_ASSERT(expr, msg) static_assert(expr, msg) 
+#define COMPILETIME_ASSERT(expr, msg) \
+	static_assert(expr, msg) 
+
+//Must switch to false to enable Release build
+#define _PROFILE \
+	false	
 
 #if (_DEBUG || _PROFILE)
 #define LOG(...) \
 	do { fprintf(stderr, __VA_ARGS__); } while (0)
 #else
-#define LOG(...) 
 #define NDEBUG
+#define LOG(...) 
 #endif
 
